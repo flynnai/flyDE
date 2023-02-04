b256f5faa1a7ac6c1f5bb938a4ee8444f2158ab7
flyai <flynnai@umich.edu>
2023-02-04 11:42:52 +0100

    Add fontawesome

--- src/FileExplorer.jsx
+++ src/FileExplorer.jsx
@@ -1,5 +1,7 @@
 import EditableField from "./EditableField";
 import styles from "./FileExplorer.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 
 function FileExplorer() {
     return (
@@ -8,7 +10,9 @@ function FileExplorer() {
                 <div className={styles.zipFilename}>
                     <EditableField />
                 </div>
-                <div className={styles.menu}></div>
+                <div className={styles.menu}>
+                    <FontAwesomeIcon icon={faEllipsisVertical} />
+                </div>
             </div>
             <div className={styles.tree}></div>
         </div>
