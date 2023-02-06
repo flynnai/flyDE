6a06d26f49c939abea1230ecc4c586218db8288d
flyai <flynnai@umich.edu>
2023-02-06 16:48:47 +0100

    Scrolling!

--- src/CodeEditor.jsx
+++ src/CodeEditor.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import styles from "./CodeEditor.module.scss";
 
 const CodeEditor = React.memo(({ content, setContent, extension }) => {
+    const inputRef = useRef(null);
+    useEffect(() => {
+        const input = inputRef.current;
+        const pre = document.querySelector("." + styles.highlightedContents);
+        if (input) {
+            const onResize = () => {
+                pre.scrollTop = input.scrollTop;
+            };
+            input.addEventListener("scroll", onResize);
+            return () => {
+                input.removeEventListener("scroll", onResize);
+            };
+        }
+    }, [inputRef.current]);
+
     return (
         <div className={styles.main}>
             <textarea
                 className={styles.editorInput}
                 value={content}
                 onChange={(e) => setContent(e.target.value)}
+                ref={inputRef}
             />
             <SyntaxHighlighter
                 language={extension}
